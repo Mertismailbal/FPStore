3b7d762344bb2acdb5feb6a755506dec440ec0c1
uluagac <ersinuluagac@gmail.com>
2025-03-21 22:05:39 +0300

    delete prop from baseentity

--- FPStore.Core/Abstracts/BaseEntity.cs
+++ FPStore.Core/Abstracts/BaseEntity.cs
@@ -11,7 +11,6 @@ namespace FPStore.Core.Abstracts
     {
         public int Id { get; set; }
         public DateTime CreatedDate { get; set; } = DateTime.Now;
-        public DateTime? UpdatedDate { get; set; }
         public Status IsActive { get; set; } = Status.Active;
     }
 }
