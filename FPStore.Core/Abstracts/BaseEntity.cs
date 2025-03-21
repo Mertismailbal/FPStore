52be46d5b8bad185d4b8a7d1e9a0f9b08d332b09
Mert İsmail Bal <balmertismail@gmail.com>
2025-03-21 22:51:34 +0300

    En son guncelleme

--- FPStore.Core/Abstracts/BaseEntity.cs
+++ FPStore.Core/Abstracts/BaseEntity.cs
@@ -12,7 +12,7 @@ namespace FPStore.Core.Abstracts
         public int Id { get; set; }
         public DateTime CreatedDate { get; set; } = DateTime.Now;
         public DateTime? UpdatedDate { get; set; }
-        public DateTime? DeleteDate { get; set; }
+        public DateTime? DeletedDate { get; set; }
         public Status IsActive { get; set; } = Status.Active;
     }
 }
